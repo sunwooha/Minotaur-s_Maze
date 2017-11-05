bd94da73f2779b49e5031e0064ed3f98128fe419
sunwooha <sunwoo.ha15@ncf.edu>
2017-11-05 17:18:49 -0500

    added corebot

---
+++ CoreBot.java
@@ -0,0 +1,18 @@
+public class CoreBot implements Robot{
+
+    public CoreBot(){
+
+    }
+
+    public ModelType getModel(){
+
+    }
+
+    public int getID(){
+
+    }
+
+    public boolean isTeamOne(){
+        
+    }
+}
