7a33de4ff7ffe7cb8b0f9df5ee207a3127f6a960
sunwooha <sunwoo.ha15@ncf.edu>
2017-11-06 17:52:02 -0500

    added the minotaur lol

--- ModelType.java
+++ ModelType.java
@@ -1,1 +1,1 @@
-public enum ModelType {CoreBot, RatBot, WolfBot, BatBot, SkunkBot, RabbitBot, FalconBot, HippoBot}
+public enum ModelType {CoreBot, RatBot, WolfBot, BatBot, SkunkBot, RabbitBot, FalconBot, HippoBot, MinotaurBot}
