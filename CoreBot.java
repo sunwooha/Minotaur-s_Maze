515724d3e8a5b2a22c8812c22fe2331e55e1fa6a
Sophie Garrett <sarasnake99@live.com>
2017-11-05 21:37:02 -0500

    Added random movement as a placeholder

--- CoreBot.java
+++ CoreBot.java
@@ -26,7 +26,25 @@ public class CoreBot implements Robot{
     }
 
     public DirType move(){
-
+		
+		// Random movement as a placeholder until we figure out something better
+		
+		int max = 4;
+		int min = 1;
+		int dir = rand.nextInt((max - min) + 1) + min;
+		
+		if (dir == 1) {
+			return DirType.North;
+		}
+		else if (dir == 2) {
+			return DirType.South;
+		}
+		else if (dir == 3) {
+			return DirType.East;
+		}
+		else if (dir == 4) {
+			return DirType.West;
+		}
     }
 
     public List<DirType> getVision(){
