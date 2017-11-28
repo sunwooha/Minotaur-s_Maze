dad25284adf208e9a98ebed94f35570982ba046e
sunwooha <sunwoo.ha15@ncf.edu>
2017-11-27 19:26:40 -0500

    indenting

--- SkunkBot.java
+++ SkunkBot.java
@@ -2,12 +2,12 @@ import java.util.*;
 
 public class SkunkBot{
 
-    public SkunkBot(int the_id, boolean teamOne){
+	public SkunkBot(int the_id, boolean teamOne){
 		model = ModelType.SkunkBot;
 		id = the_id;
 		is_teamOne = teamOne;
-		
+
 		coinTypes.add(CoinType.Bronze);
 		coinTypes.add(CoinType.Silver);
-    }
+		}
 }
--- WolfBot.java
+++ WolfBot.java
@@ -2,12 +2,12 @@ import java.util.*;
 
 public class WolfBot{
     
-    public WolfBot(int the_id, boolean teamOne){
-		model = ModelType.WolfBot;
-		id = the_id;
-		is_teamOne = teamOne;
-		
-		coinTypes.add(CoinType.Bronze);
-		coinTypes.add(CoinType.Silver);
-    }
+public WolfBot(int the_id, boolean teamOne){
+	model = ModelType.WolfBot;
+	id = the_id;
+	is_teamOne = teamOne;
+			
+	coinTypes.add(CoinType.Bronze);
+	coinTypes.add(CoinType.Silver);
+	}
 }
