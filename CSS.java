367c48c52989f3b9374380b45f7d55c0fad592df
sunwooha <sunwoo.ha15@ncf.edu>
2017-11-28 20:14:49 -0500

    fiddling with vision function

--- CSS.java
+++ CSS.java
@@ -116,14 +116,14 @@ public class CSS implements PlayerTeam{
 		}
 		return currentLoc;
 	}
-	/**
-	 * Need a way to look 2 spaces N,S,W,E from current position
-	 */
-	public List<Location> vision(Robot r, Location currentLoc){
-		List<Location> the_vision = new ArrayList<Location>();
-		int x = currentLoc.getX();
-		int y = currentLoc.getY();
 
+	public List<Location> vision(Robot r, List<Location> information){
+		List<Location> the_vision = new ArrayList<Location>();
+		for(Location loc: information){
+			if(loc.isScanned() == true){
+				the_vision.add(loc);
+			}
+		}
 		return the_vision;
 	}
 }
